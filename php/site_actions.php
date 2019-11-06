e2a4f813e9636bd18d46c2fbe6ae063079c59b2e
Desmond Springer <desmond_k_springer@homedepot.com>
2019-11-06 15:01:25 -0500

    added new code with additional tabs to e-portfolio. Although this should have been pushed into develop branch, I am pushing into master to clean desktop

--- php/site_actions.php
+++ php/site_actions.php
@@ -2,12 +2,12 @@
 if(isset($_POST['submit'])){
     $to = "dspringer08@yahoo.com"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
-    $fname = $_POST['fname'];
-    $lname = $_POST['lname'];
-    $subject = "You have a(n) " + $_POST['inquiry'] + " from " + $_POST['fname'] + " " + $_POST['lname'];
+    $fullname = $_POST['fullname'];
+    $number = $_POST['number'];
+    $subject = "You have a(n) " + $_POST['inquiry'] + " from " + $_POST['fullname'];
     $subject2 = "Copy of your form submission";
-    $message = $fname . " " . $lname . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $fname . "\n\n" . $_POST['message'];
+    $message = $fullname " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message.\n\n" . $_POST['message'];
 
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
